4288a56522504694b30d1601551bf3b1e8235260
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-04-26 19:27:46 -0400

    Add highlight to sidebar

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-export default function GenreList({ onSelectGenre }: Props) {
+export default function GenreList({ onSelectGenre, selectedGenre }: Props) {
   const { data, isLoading, error } = useGenres();
 
   if (error) null;
@@ -21,7 +22,12 @@ export default function GenreList({ onSelectGenre }: Props) {
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-              <Button onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">
+              <Button
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+              >
                 {genre.name}
               </Button>
             </HStack>
