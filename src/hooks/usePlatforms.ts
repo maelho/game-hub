3cd953dc68b8020274e935909c416f1500a65174
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-04-26 23:46:13 -0400

    simplify time calculations

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import platforms from "../data/platforms";
 import APICLient from "../services/api-client";
 
@@ -14,7 +15,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiCLient.getAll,
-    staleTime: 24 + 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"),
     initialData: { count: platforms.length, results: platforms },
   });
 
