7d61bb21bd8b4aca1da911632db454ff2261cd39
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-04-26 22:33:35 -0400

    add react query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -6,6 +8,12 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({data: platforms, error: "", isLoading: false})
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () => apiClient.get<FetchResponse<Platform>>("/platforms").then((res) => res.data),
+    staleTime: 24 + 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
