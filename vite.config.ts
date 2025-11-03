3aca77b10138a228bd7402616744ca8d38962f77
maelho <maehlo@users.noreply.github.com>
2025-11-03 10:53:44 -0400

    replace react-router with @tanstack/react-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -15,6 +16,10 @@ export default defineConfig({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
+    }),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
     }),
     tailwindcss(),
   ],
