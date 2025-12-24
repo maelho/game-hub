394d947b4ea274dd17aec434c65662f69927f073
maelho <maehlo@users.noreply.github.com>
2025-12-24 19:05:03 -0400

    chore: border to the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -61,7 +61,7 @@ export default function Navbar() {
   }
 
   return (
-    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container mx-auto flex h-16 items-center gap-4 px-4 sm:px-6">
         <Link
           className="flex items-center gap-2 font-bold text-xl transition-colors hover:text-primary"
